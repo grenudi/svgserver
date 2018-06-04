ee22caa1daf8cb6655970d24cb0f521d0ee947ab
grenudi <idunerg@gmail.com>
2018-06-04 14:48:07 +0300

    this shit isn't working, and i don't won't to create full web browser mock

--- primitivator/workers/worker.js
+++ primitivator/workers/worker.js
@@ -3,21 +3,20 @@ const Fs = require("fs");
 const {join} = require("path");
 const config = require("../config-custom.json");
 
-function convert(file,config){
+function convert(file){
 	return new Promise((ok)=>{
-		const path = join(__dirname,"../pending/"+file);
-		app.convert(path,config)
-			.then(svg=>{
-				Fs.writeFile(join(__dirname,"../done/"+file.split(".")[0]+".svg"), svg ,(err)=>{
-					if(err) console.error("ERROR! cannot write SVG");
-					ok("done");
-				});
-			})
-			.catch((err)=>console.log("ERROR! app.convert\n",err));
+		const path = join(__dirname,"./pending/"+file);
+		app.convert(file);
+		// .then(svg=>{
+		// 	Fs.writeFile(join(__dirname,"../done/"+file.split(".")[0]+".svg"), svg ,(err)=>{
+		// 		if(err) console.error("ERROR! cannot write SVG");
+		// 		ok("done");
+		// 	});
+		// })
+		// .catch((err)=>console.log("ERROR! app.convert\n",err));
 	});
 }
-
-convert(join(__dirname,"../pending/Tulips.jpg"),config);
+convert("Tulips.jpg");
 // receive message from master process
 process.on("message",  (file,config) => {
 	convert(file,config).then(msg=>process.send(msg));
